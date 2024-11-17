89e620e320d74ba0c58153e900158081d1e7b30a
Ankit Jha <professionalaj209@gmail.com>
2024-11-17 18:09:18 +0530

    minor issues fixed

--- src/components/AllNotes.jsx
+++ src/components/AllNotes.jsx
@@ -35,7 +35,7 @@ const AllNotes = () => {
         <div key={note.id} className='bg-gray-700 text-gray-200 rounded-lg px-5 py-2 mb-5 w-full'>
           <h2 className='text-2xl font-bold text-gray-400 text-left'>{note.title}</h2>
           <div className='flex flex-col sm:justify-between mt-5 gap-5 sm:flex-row'>
-            <h5 className='text-left'>{`${note.content.substring(1,100)}...`}</h5>
+            <h5 className='text-left'>{`${note.content.substring(0,100)}...`}</h5>
             <div className='flex justify-center items-center gap-10 sm:gap-5'>
                 <NavLink className='text-green-800 bg-green-500 hover:bg-green-700 hover:text-black rounded-md text-lg p-1' to={`/?noteId=${note.id}`}><CiEdit/></NavLink>
                 <NavLink className='text-gray-800 bg-blue-500 hover:bg-blue-700 rounded-md text-lg p-1' to={`${note.id}`} title={note.title}><FaEye/></NavLink>
